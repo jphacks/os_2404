8a693f2f863a3ef9dcbb314ef98c410f4c6731bd
seipan <yamasakipann0218@gmail.com>
2024-10-26 16:05:48 +0900

    feat : all

--- server/go.mod
+++ server/go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/oklog/ulid v1.3.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	golang.org/x/oauth2 v0.23.0
 )
---
+++ server/usecase/user.go
@@ -0,0 +1,60 @@
+package usecase
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/jphacks/os_2404/domain/entity"
+	"github.com/jphacks/os_2404/domain/repository"
+)
+
+var _ IUserUsecase = &UserUsecase{}
+
+type UserUsecase struct {
+	repo repository.IUserRepository
+}
+
+type IUserUsecase interface {
+	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
+	DeleteUser(ctx context.Context, id string) error
+	GetUser(ctx context.Context, id string) (*entity.User, error)
+}
+
+func NewUserUsecase(repo repository.IUserRepository) IUserUsecase {
+	return &UserUsecase{
+		repo: repo,
+	}
+}
+
+func (uu *UserUsecase) CreateUser(ctx context.Context, user *entity.User) (*entity.User, error) {
+	if user.Name == "" {
+		return nil, fmt.Errorf("user name empty")
+	}
+	if user.Img == "" {
+		return nil, fmt.Errorf("img name empty")
+	}
+
+	resuser, err := uu.repo.CreateUser(ctx, user)
+	return resuser, err
+}
+
+func (uu *UserUsecase) DeleteUser(ctx context.Context, id string) error {
+	if id == "" {
+		return fmt.Errorf("id empty")
+	}
+	_, err := uu.GetUser(ctx, id)
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("The user with this id does not exist")
+	}
+	err = uu.repo.DeleteUser(ctx, id)
+	return err
+}
+
+func (uu *UserUsecase) GetUser(ctx context.Context, id string) (*entity.User, error) {
+	if id == "" {
+		return nil, fmt.Errorf("id empty")
+	}
+	resuser, err := uu.repo.GetUser(ctx, id)
+	return resuser, err
+}
